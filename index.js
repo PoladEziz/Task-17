90c654014cc2d5acefc76c1dd44b1e323443302b
PoladEziz <dspolad26@gmail.com>
2023-08-17 11:20:34 +0400

    incomplated task

---
+++ index.js
@@ -0,0 +1,7 @@
+let btn=document.querySelector('button')
+let lists=document.querySelectorAll('li')
+let rightBox=document.querySelector('.right-box')
+btn.addEventListener('click',change)
+function change() {
+
+}
